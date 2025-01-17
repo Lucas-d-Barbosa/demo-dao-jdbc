600162060dc0b15cec9e2d5f7a809919ca17dd1c
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-01-17 16:53:09 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,7 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 		Department obj = new Department("Books", 1);
-		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = new Seller(1, "Lucas", "lucas@gmail.com", sdf.parse("05/07/2002"), 2500.00, obj);
 		System.out.println(obj);
 		System.out.println(seller);
