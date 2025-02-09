40c19336b466ddd3790e206a4148ebc4408ea926
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-02-08 21:02:05 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		Seller seller = new Seller(1, "Lucas Barbosa", "lucas@gmail.com",sdf.parse("05/07/2002") ,3000.00, obj);
+		System.out.println(seller);
 	}
-}
+}	
